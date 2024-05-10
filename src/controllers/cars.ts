70e1004172086412e929135cd89ee5cc9ae5c8ea
Luanbarradas <barradas.luk@gmail.com>
2024-05-10 16:40:21 -0300

    feat(routes): listCars working

--- src/controllers/cars.ts
+++ src/controllers/cars.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from "express";
+import { knex } from "../database/connection";
 
-export const listCars = async (req: Request, res: Response) => {};
+// forma de tratar o erro, no entanto não é ideal por retornar informações sensiveis
+// type Erro = {
+//   message: string;
+// };
+
+// export const listCars = async (_: Request, res: Response) => {
+//   try {
+//     const cars = await knex("carros");
+//   } catch (error) {
+//     const erro = error as Erro;
+//     return res.status(400).json({ message: erro.message });
+//   }
+// };
+
+// quando um parâmetro não for usado, por convenção deve-se colocar "_" no lugar
+export const listCars = async (_: Request, res: Response) => {
+  try {
+    const cars = await knex("carros");
+    return res.json(cars);
+  } catch {
+    return res.status(500).json({ message: "Erro interno do servidor." });
+  }
+};
 
 export const detailCars = async (req: Request, res: Response) => {};
 
